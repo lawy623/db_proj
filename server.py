7c6d6202faae8f8b682acebdf6577e7deaff8465
lawy623 <yl4003@columbia.edu>
2019-04-05 13:36:10 -0400

    add player null handle

--- server.py
+++ server.py
@@ -417,7 +417,7 @@ def add_player():
     print player.cname
     print player.nation
     if player.number is None:
-        return redirect("insert_null")
+        return render_template("insert_null.html")
 
     g.conn.execute('INSERT INTO player VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)', player.number, player.age, player.position, player.price, player.height, player.nationality, player.name, player.since, player.cname, player.nation, player.level, player.foot)
     return redirect(url_for('team', nation = player.nation, level = player.level, cname = player.cname))
