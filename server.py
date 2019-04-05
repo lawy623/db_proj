a6a263ae6d2dd7568b193a320acfd9c4976298e0
lawy623 <yl4003@columbia.edu>
2019-04-05 16:04:49 -0400

    test add

--- server.py
+++ server.py
@@ -450,9 +450,9 @@ def add_record():
     # guest scores
     print "guest"
     for i in range(5):
-        player = request.form['home_score'+str(i)]
-        number_score = request.form['home_goal_number'+str(i)]
-        own_goal = request.form['home_own_goal'+str(i)]
+        player = request.form['guest_score'+str(i)]
+        number_score = request.form['guest_goal_number'+str(i)]
+        own_goal = request.form['guest_own_goal'+str(i)]
         if player != '':
             print """{} x{}({})""".format(player, number_score, own_goal)
 
