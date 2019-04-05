0193fbd58fd215f33a31d0da817f27105d9ef87f
lawy623 <yl4003@columbia.edu>
2019-04-05 16:09:44 -0400

    test add

--- server.py
+++ server.py
@@ -439,24 +439,20 @@ def add_record():
     #print """Match {0}: {1} vs {2} -- [{3} {4}] """.format(mid, home_cname, guest_cname, nation, level)
 
     # home scores
-    print "home"
     for i in range(5):
-        print "home"+str(i)
         player = request.form['home_score'+str(i)]
         number_score = request.form['home_goal_number'+str(i)]
         own_goal = request.form['home_own_goal'+str(i)]
         if player != '':
-            print """{} x{}({})""".format(player, number_score, own_goal)
+            print """home {} x{}({})""".format(player, number_score, own_goal)
 
     # guest scores
-    print "guest"
     for i in range(5):
-        print "guest"+str(i)
         player = request.form['guest_score'+str(i)]
         number_score = request.form['guest_goal_number'+str(i)]
         own_goal = request.form['guest_own_goal'+str(i)]
         if player != '':
-            print """{} x{}({})""".format(player, number_score, own_goal)
+            print """guest {} x{}({})""".format(player, number_score, own_goal)
 
 
     return redirect(url_for('match', mid = mid))
